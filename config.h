18a52d8228c1be4ebf2cf09486715c80e83d453a
Rohit Karunakaran <rohitkarun@gmail.com>
2021-05-11 15:29:35 +0530

    Working config with the fullscreen mode \n It has a problem because Mod+f was mapped to togglefullscreen and enabling floating mode

--- config.h
+++ config.h
@@ -1,6 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 
-
 /* appearance */
 static const unsigned int borderpx  = 10;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -79,13 +78,13 @@ static Key keys[] = {
 	{ MODKEY,                               XK_l,                      setmfact,       {.f = +0.05} },
 	{ MODKEY|ShiftMask,                     XK_Return,                 zoom,           {0} },
 	{ MODKEY,                               XK_Tab,                    view,           {0} },
-	{ MODKEY|ShiftMask,                     XK_q,                      killclient,     {0} },
-	{ MODKEY,                               XK_t,                      setlayout,      {.v = &layouts[0]} },
+    { MODKEY|ShiftMask,                     XK_q,                      killclient,     {0} },
+    { MODKEY,                               XK_t,                      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                               XK_f,                      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                               XK_m,                      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                               XK_space,                  setlayout,      {0} },
 	{ MODKEY|ShiftMask,                     XK_space,                  togglefloating, {0} },
-	{ MODKEY,                               XK_f,                      togglefullscr,  {0} },
+	{ MODKEY|ShiftMask,                     XK_f,                      togglefullscr,  {0} },
 	{ MODKEY,                               XK_0,                      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,                     XK_0,                      tag,            {.ui = ~0 } },
 	{ MODKEY,                               XK_comma,                  focusmon,       {.i = -1 } },
--- dwm.c
+++ dwm.c
@@ -84,6 +84,7 @@ typedef struct {
 
 typedef struct Monitor Monitor;
 typedef struct Client Client;
+
 struct Client {
 	char name[256];
 	float mina, maxa;
@@ -1135,6 +1136,7 @@ motionnotify(XEvent *e)
 	mon = m;
 }
 
+
 void
 movemouse(const Arg *arg)
 {
@@ -1146,7 +1148,7 @@ movemouse(const Arg *arg)
 
 	if (!(c = selmon->sel))
 		return;
-	if (c->isfullscreen) /* no support moving fullscreen windows by mouse */
+	if (c->isfullscreen)  /*no support moving fullscreen windows by mouse */
 		return;
 	restack(selmon);
 	ocx = c->x;
@@ -1482,7 +1484,7 @@ setfullscreen(Client *c, int fullscreen)
 		c->oldstate = c->isfloating;
 		c->oldbw = c->bw;
 		c->bw = 0;
-		c->isfloating = 1;
+		//c->isfloating = 1;
 		resizeclient(c, c->mon->mx, c->mon->my, c->mon->mw, c->mon->mh);
 		XRaiseWindow(dpy, c->win);
 	} else if (!fullscreen && c->isfullscreen){
