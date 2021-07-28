9f15189402f443365a747a43512dce0dd9470090
Rohit Karunakaran <rohitkarun@gmail.com>
2021-07-28 13:34:51 +0530

    Cleaned the dwm.c file
    Modified the patches such a way that there is minimal changes to the
    original dwm.c file
    TODO: drw.c

--- patches.c
+++ patches.c
@@ -1,20 +1,19 @@
-
-
-/*From: Soenke Lambert <s.lambert@mittwald.de> */
+/*
+// From: Soenke Lambert <s.lambert@mittwald.de>
 static void togglefullscr(const Arg *arg);
 
-/*From: From: Miles Alan <m@milesalan.com> */
+// From: From: Miles Alan <m@milesalan.com>
 static void inplacerotate(const Arg *arg);
 
-/*From:MLquest8 <miskuzius@gmail.com>*/ 
+/*From:MLquest8 <miskuzius@gmail.com>
 static void attachtop(Client *c);
 
-/* 
   Maciej Janicki mail@macjanicki.eu
   David Julien swy7ch@protonmail.com (20200504-b2e1dfc port)
-  Klein Bottle kleinbottle4@gmail.com (dwm-fullgaps-toggle...) 
-*/
+  Klein Bottle kleinbottle4@gmail.com (dwm-fullgaps-toggle...)
+
 static void setgaps(const Arg *arg);
+*/
 
 /*From: Soenke Lambert <s.lambert@mittwald.de> */
 void
@@ -94,15 +93,15 @@ inplacerotate(const Arg *arg)
     focus(c);
 }
 
-/*From:MLquest8 <miskuzius@gmail.com>*/ 
-void 
+/*From:MLquest8 <miskuzius@gmail.com>*/
+void
 attachtop(Client *c)
 {
     int n;
     Monitor *m = selmon;
     Client *below;
 
-    for (n = 1, below = c->mon->clients; 
+    for (n = 1, below = c->mon->clients;
             below && below->next && (below->isfloating || !ISVISIBLEONTAG(below,c->tags) || n != m->nmaster);
             n = below->isfloating || !ISVISIBLEONTAG(below,c->tags)? n + 0 : n + 1, below=below->next);
     c->next=NULL;
@@ -122,4 +121,93 @@ setgaps(const Arg *arg)
     else
         selmon->gappx += arg->i;
     arrange(selmon);
+}
+
+
+
+void
+buttonpress(XEvent *e)
+{
+	unsigned int i, x, click;
+	Arg arg = {0};
+	Client *c;
+	Monitor *m;
+	XButtonPressedEvent *ev = &e->xbutton;
+
+	click = ClkRootWin;
+	/* focus monitor if necessary */
+	if ((m = wintomon(ev->window)) && m != selmon) {
+		unfocus(selmon->sel, 1);
+		selmon = m;
+		focus(NULL);
+	}
+	if (ev->window == selmon->barwin) {
+		i = x = 0;
+		do
+			x += TEXTW(tags[i]);
+		while (ev->x >= x && ++i < LENGTH(tags));
+		if (i < LENGTH(tags)) {
+			click = ClkTagBar;
+			arg.ui = 1 << i;
+		} else if (ev->x < x + blw)
+			click = ClkLtSymbol;
+		else if (ev->x > selmon->ww - (int)TEXTW(stext))
+			click = ClkStatusText;
+		else
+			click = ClkWinTitle;
+	} else if ((c = wintoclient(ev->window))) {
+		focus(c);
+		restack(selmon);
+		XAllowEvents(dpy, ReplayPointer, CurrentTime);
+		click = ClkClientWin;
+	}
+	for (i = 0; i < LENGTH(buttons); i++)
+		if (click == buttons[i].click && buttons[i].func && buttons[i].button == ev->button
+		&& CLEANMASK(buttons[i].mask) == CLEANMASK(ev->state))
+			buttons[i].func(click == ClkTagBar && buttons[i].arg.i == 0 ? &arg : &buttons[i].arg);
+}
+
+Monitor *
+createmon(void)
+{
+	Monitor *m;
+
+	m = ecalloc(1, sizeof(Monitor));
+	m->tagset[0] = m->tagset[1] = 1;
+	m->mfact = mfact;
+	m->nmaster = nmaster;
+	m->showbar = showbar;
+	m->topbar = topbar;
+  m->gappx = gappx;
+	m->lt[0] = &layouts[0];
+	m->lt[1] = &layouts[1 % LENGTH(layouts)];
+	strncpy(m->ltsymbol, layouts[0].symbol, sizeof m->ltsymbol);
+	return m;
+}
+
+//Gapped Tiling
+void
+tile(Monitor *m)
+{
+	unsigned int i, n, h, mw, my, ty;
+	Client *c;
+
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	if (n == 0)
+		return;
+
+	if (n > m->nmaster)
+		mw = m->nmaster ? m->ww * m->mfact : 0;
+	else
+		mw = m->ww - m->gappx;
+	for (i = 0, my = ty = m->gappx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+		if (i < m->nmaster) {
+			h = (m->wh - my ) / (MIN(n,m->nmaster)-i) - m->gappx;
+			resize(c, m->wx + m->gappx, m->wy + my, mw - (2*c->bw) - m->gappx, h - (2*c->bw), 0);
+			my += HEIGHT(c) + m->gappx;
+		} else {
+			h = (m->wh - ty ) / (n - i) - m->gappx;
+			resize(c, m->wx + mw + m->gappx, m->wy + ty, m->ww - mw - (2*c->bw) - 2*m->gappx, h - (2*c->bw), 0);
+			ty += HEIGHT(c) + m->gappx;
+		}
 }
