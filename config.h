742e765859d334814ec469495d45cb4957063da0
Rohit Karunakaran <rohitkarun@gmail.com>
2021-07-28 09:53:02 +0530

    Changed fonts and added new application rules
    Changed gap size to 0

--- config.h
+++ config.h
@@ -2,13 +2,16 @@
 
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
-static const unsigned int snap      = 5;       /* snap pixel */
-static const unsigned int gappx     = 8;
+static const unsigned int snap      = 3;       /* snap pixel */
+static const unsigned int gappx     = 0;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = {"monospace:size=10","FontAwesome:size=9" };
+//static const char *fonts[]          = {"monospace:size=10","Nerd Font:size=11","Font Awesome 5:size=9" };
+//static const char *fonts[]          = {"Ubuntu:style=Light Italic:size=11","Symbols Nerd Font:style=2048-em:size=13" };
+static const char *fonts[]          = {"Sarasa Mono K:size=11","Symbols Nerd Font:style=2048-em:size=13" };
+//static const char *fonts[]          = {"ProggySquare:size=9","Font Awesome 5:size=9" };
 static const int focusonwheel       = 1;
-static const char dmenufont[]       = "monospace:size=10";
+static const char dmenufont[]       = "Sarasa Mono K:size=12";
 static const char normbg[]          = "#2e3440";
 static const char normborder[]      = "#2e3440";
 static const char normfg[]          = "#d8dee9";
@@ -32,7 +35,6 @@ static const Rule rules[] = {
 	/* class                     instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",                      NULL,       NULL,       0,            1,           -1 },
 	{ "feh",                       NULL,       NULL,       0,            1,           -1 },
-	{ "Sxiv",                      NULL,       NULL,       0,            1,           -1 },
 	{ "vlc",                       NULL,       NULL,       0,            1,           -1 },
     { "Pavucontrol",               NULL,       NULL,       0,            1,           -1 },
     { "Tor Browser",               NULL,       NULL,       1 << 8,       1,           -1 },
@@ -52,7 +54,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "< >",      NULL },    /* no layout function means floating behavior */
+	{ "<><",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -69,13 +71,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[]         =     { "rofi", "-show", "run", NULL };
+//static const char *dmenucmd[]         =     { "rofi", "-show", "run", NULL };
+static const char *dmenucmd[]         =     { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbg , "-nf", normfg, "-sb", selbg,  "-sf", selfg, NULL };
 static const char *termcmd[]          =     { "myterminal", NULL }; //It is a custom bash command that launches your terminal with extra arguments any (so you dont need to recompile this stuff again)
 static const char *volinccmd[]        =     { "volchange","inc","3",NULL };
 static const char *voldeccmd[]        =     { "volchange","dec","3",NULL };
 static const char *brightnessup[]     =     { "xbacklight", "-inc","10" ,NULL };
 static const char *brightnessdown[]   =     { "xbacklight", "-dec","10" ,NULL };
-static const char *screenshotcmd[]    =     { "scrot","SS_%Y%m%d%H%M%S_$wx$h.png","-q","25", "-e", "mv $f ~/Pictures/Screenshots/" ,NULL };
+static const char *screenshotcmd[]    =     { "scrot","SS_%Y%m%d%H%M%S_$wx$h.png","-q","30", "-e", "mv $f ~/Pictures/Screenshots/" ,NULL };
+static const char *browser[]          =     { "firefox", "--no-remote", NULL };
+static const char *firefoxProfile[]   =     { "firefox", "--no-remote","--ProfileManager", NULL };
 
 //static const char *volinccmd[]        =     { "amixer","sset","PCM","playback","5+",NULL };
 //static const char *voldeccmd[]        =     { "amixer","sset","PCM","playback","5-",NULL };
@@ -94,14 +99,14 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,                     XK_k,                      inplacerotate,  {.i = +2 } },
 	{ MODKEY|ShiftMask,                     XK_h,                      inplacerotate,  {.i = -1 } },
 	{ MODKEY|ShiftMask,                     XK_l,                      inplacerotate,  {.i = +1 } },
-	{ MODKEY,                               XK_h,                      setmfact,       {.f = -0.05} },
-	{ MODKEY,                               XK_l,                      setmfact,       {.f = +0.05} },
+	{ MODKEY,                               XK_h,                      setmfact,       {.f = -0.05 } },
+	{ MODKEY,                               XK_l,                      setmfact,       {.f = +0.05 } },
 	{ MODKEY,                               XK_Return,                 zoom,           {0} },
 	{ MODKEY,                               XK_Tab,                    view,           {0} },
     { MODKEY|ShiftMask,                     XK_q,                      killclient,     {0} },
-    { MODKEY,                               XK_t,                      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                               XK_f,                      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                               XK_m,                      setlayout,      {.v = &layouts[2]} },
+    { MODKEY,                               XK_t,                      setlayout,      {.v = &layouts[0] } },
+	{ MODKEY,                               XK_f,                      setlayout,      {.v = &layouts[1] } },
+	{ MODKEY,                               XK_m,                      setlayout,      {.v = &layouts[2] } },
 	{ MODKEY,                               XK_space,                  setlayout,      {0} },
 	{ MODKEY|ShiftMask,                     XK_space,                  togglefloating, {0} },
 	{ MODKEY|ShiftMask,                     XK_f,                      togglefullscr,  {0} },
@@ -114,19 +119,20 @@ static Key keys[] = {
 	{ MODKEY,                               XK_minus,                  setgaps,        {.i = -1 } },
 	{ MODKEY,                               XK_equal,                  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,                     XK_equal,                  setgaps,        {.i = 0 } },
-	{ MODKEY,                               XK_b,                      spawn,          SHCMD("firefox") },
+	{ MODKEY,                               XK_b,                      spawn,          {.v = browser } },
+	{ MODKEY|ControlMask,                   XK_b,                      spawn,          {.v = firefoxProfile } },
 	{ ControlMask|Mod1Mask,                 XK_l,                      spawn,          SHCMD("lock") },
 	{ MODKEY|ShiftMask,/*Normie Nightmare*/ XK_n,                      spawn,          SHCMD("xinput disable 'DELL083F:00 04F3:3088 Touchpad'") },
     { MODKEY,                               XK_r,                      spawn,          SHCMD("xset r rate 250 25") },
 	{ 0,                                    0x1008ff13,                spawn,          {.v = volinccmd} },
 	{ 0,                                    0x1008ff11,                spawn,          {.v = voldeccmd} },
 	{ 0,                                    0x1008ff12,                spawn,          {.v = (const char*[]){"volchange","toggle",NULL} } },
-	{ 0,                                    0x1008ff02,                spawn,          {.v = brightnessup} },
+	{ 0,                                    0x1008ff02,                spawn,          {.v = brightnessup } },
 	{ 0,                                    0x1008ff03,                spawn,          {.v = brightnessdown} },
-	{ 0,                                    0x1008ff14,                spawn,          {.v = (const char*[]){"mpc","toggle",NULL}} },
-	{ 0,                                    0x1008ff17,                spawn,          {.v = (const char*[]){"mpc","next",NULL}} },
-	{ 0,                                    0x1008ff16,                spawn,          {.v = (const char*[]){"mpc","prev",NULL}} },
-    { 0,                                    0xff61,                    spawn,          {.v = screenshotcmd} },
+	{ 0,      /*Play Button    */           0x1008ff14,                spawn,          {.v = (const char*[]){"mpc","toggle",NULL}} },
+	{ 0,      /*Forward button */           0x1008ff17,                spawn,          {.v = (const char*[]){"mpc","next",NULL}} },
+	{ 0,      /*Rewind Button  */           0x1008ff16,                spawn,          {.v = (const char*[]){"mpc","prev",NULL}} },
+    { 0,                                    0xff61,                    spawn,          {.v = screenshotcmd } },
 	TAGKEYS(                                XK_1,                      0)
 	TAGKEYS(                                XK_2,                      1)
 	TAGKEYS(                                XK_3,                      2)
